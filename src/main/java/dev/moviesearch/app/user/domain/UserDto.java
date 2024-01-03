3e1bda5ffb07ac688767f4f2e80a0ad4f38c813f
ihayeong <ihayeong@218.38.137.27>
2024-01-04 00:39:21 +0900

    (로그인 기능. 로직 헤매는 중)

--- src/main/java/dev/moviesearch/app/user/domain/UserDto.java
+++ src/main/java/dev/moviesearch/app/user/domain/UserDto.java
@@ -1,6 +1,5 @@
 package dev.moviesearch.app.user.domain;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
